d53f2a3af45e8ad38a382b2bd961600309437a79
bhashitha1428 <bhashithamiyurantha.bmr@gmail.com>
2020-09-02 09:24:17 +0530

    FirstCommit

---
+++ src/Config.ts
@@ -0,0 +1,8 @@
+export const config = {
+    appId:'15a87dc9-be16-4c0f-b24c-f13293a9f4bc',
+    redirectUri: 'http://localhost:3000',
+    scopes: [
+      'user.read',
+      'calendars.read'
+    ]
+  };
